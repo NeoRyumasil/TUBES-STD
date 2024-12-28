e45906ce58dc6834d8ed3757b13c0bc275727a87
Muhammad Alvin Ababil <96902735+NeoRyumasil@users.noreply.github.com>
2024-12-28 11:07:51 +0700

    Update main.cpp

--- AplikasiDokter/main.cpp
+++ AplikasiDokter/main.cpp
@@ -9,7 +9,7 @@ int main()
     listPasien lPasien;
     listRelasi lRelasi;
     string namaDokter;
-    int choice;
+    int select;
 
     createListDokter(lDokter);
     createListPasien(lPasien);
@@ -21,7 +21,40 @@ int main()
          cout << "===============================================================" << endl;
          cout << "Total Dokter: " << dokterCounter(lDokter) << endl;
          cout << "Total Pasien: " << pasienCounter(lPasien) << endl;
-         choice = mainMenu();
+         select = mainMenu();
 
+        switch(select){
+        case 1:
+            insertDataDokter(lDokter);
+            break;
+        case 2:
+            insertDataPasien(lPasien, lDokter, lRelasi);
+            break;
+        case 3:
+            showDataDokter(lDokter);
+            break;
+        case 4:
+            showDataPasien(lPasien);
+            break;
+        case 5:
+            searchDataDokter(lDokter, lRelasi);
+            break;
+        case 6:
+            searchDataPasien(lPasien);
+            break;
+        case 7:
+            deleteDokter(lDokter, lRelasi, lPasien);
+            break;
+        case 8:
+            deletePasien(lPasien, lRelasi);
+            break;
+        case 9:
+            showAllData(lDokter, lPasien, lRelasi);
+            break;
+        }
+
+        if (select > 0 && choice <= 9){
+           goto start;
+        }
 }
 
